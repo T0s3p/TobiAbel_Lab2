a17a8a2c6e9706925a21dd907a2cef2f960d9434
Tobi <tobiabel22@gmail.com>
2024-02-24 20:31:03 -0500

    Comp367 Lab2 Jenkins

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,13 +1,14 @@
-package $org.example;
+package org.example;
+
+import java.io.IOException;
+import java.time.LocalTime;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to COMP367!" );
-    }
-}
+      
