b3fc2b0363b9072a3851d81beb555c20a420fae2
Tobi <tobiabel22@gmail.com>
2024-02-24 20:38:00 -0500

    Comp367 Lab2 Jenkins

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -11,4 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-      
+        LocalTime currentTime = LocalTime.now();
+
+        String greeting;
+        if (currentTime.isBefore(LocalTime.NOON)) {
+            greeting = "Good morning";
+        } else {
+            greeting = "Good afternoon";
+        }
+
+        // Display the greeting message
+        System.out.println(greeting + ", <your name>, Welcome to COMP367");
+    }
+}
